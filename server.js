0983a2fc70bc30e52d7a29039111fd394c43487e
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2024-06-07 10:03:36 -0400

    style server.js indentation

--- server.js
+++ server.js
@@ -14,28 +14,28 @@ const port = process.env.PORT ? process.env.PORT : '3000';
 mongoose.connect(process.env.MONGODB_URI);
 
 mongoose.connection.on('connected', () => {
-  console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
+	console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
 });
 
 app.use(express.urlencoded({ extended: false }));
 app.use(methodOverride('_method'));
 // app.use(morgan('dev'));
 app.use(
-  session({
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: true,
-  })
+	session({
+		secret: process.env.SESSION_SECRET,
+		resave: false,
+		saveUninitialized: true,
+	})
 );
 
 app.get('/', (req, res) => {
-  res.render('index.ejs', {
-    user: req.session.user,
-  });
+	res.render('index.ejs', {
+		user: req.session.user,
+	});
 });
 
 app.use('/auth', authController);
 
 app.listen(port, () => {
-  console.log(`The express app is ready on port ${port}!`);
+	console.log(`The express app is ready on port ${port}!`);
 });
