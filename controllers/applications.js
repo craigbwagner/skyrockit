8207bc7e0cac1f8d47031f434e39434ca9e151f5
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2024-06-07 16:10:54 -0400

    add deleteApplication controller

--- controllers/applications.js
+++ controllers/applications.js
@@ -36,4 +36,16 @@ async function show(req, res) {
     }
 }
 
-module.exports = { index, new: newAppForm, create, show };
+async function deleteApplication(req, res) {
+	try {
+		const currentUser = await User.findById(req.session.user._id);
+		currentUser.applications.id(req.params.applicationId).deleteOne();
+		await currentUser.save();
+		res.redirect(`/users/${currentUser._id}/applications`);
+	} catch (error) {
+		console.log(error);
+		res.redirect('/');
+	}
+}
+
+module.exports = { index, new: newAppForm, create, show, deleteApplication };
