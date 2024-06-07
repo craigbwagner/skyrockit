f6d983a56658e001c1a24494a6187733213cd7b6
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2024-06-07 15:11:25 -0400

    add application creation controller

--- controllers/applications.js
+++ controllers/applications.js
@@ -12,4 +12,16 @@ function newAppForm(req, res) {
 	res.render('applications/new.ejs');
 }
 
-module.exports = { index, new: newAppForm };
+async function create(req, res) {
+	try {
+		const currentUser = await User.findById(req.session.user._id);
+		currentUser.applications.push(req.body);
+		await currentUser.save();
+		res.redirect(`/users/${currentUser._id}/applications`);
+	} catch (err) {
+		console.log(err);
+		res.redirect('/');
+	}
+}
+
+module.exports = { index, new: newAppForm, create };
