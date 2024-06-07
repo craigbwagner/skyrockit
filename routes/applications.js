3b7c35e46afe969e3aed088d50c70c3c2ab7e126
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2024-06-07 15:29:18 -0400

    add show page route

--- routes/applications.js
+++ routes/applications.js
@@ -5,5 +5,6 @@ const applicationCtrl = require('../controllers/applications.js');
 router.get('/', applicationCtrl.index);
 router.get('/new', applicationCtrl.new);
 router.post('/', applicationCtrl.create);
+router.get('/:applicationId', applicationCtrl.show);
 
 module.exports = router;
